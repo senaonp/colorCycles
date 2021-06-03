07b71b46977881c18ad3e47e3ba433f149e29571
senaonp <71605688+senaonp@users.noreply.github.com>
2021-06-02 23:09:52 -0400

    specify additional typing to vars

--- typescript-version/colorCycles.ts
+++ typescript-version/colorCycles.ts
@@ -18,7 +18,7 @@ class ColorCycles {
 		setInterval(function() {
 				thisObj.currentColorIndex = (thisObj.currentColorIndex + 1 == thisObj.colors.length) ? 0 : thisObj.currentColorIndex + 1;
 		}, this.pause);
-		for (let i=0; i<this.elemQueries.length; i+=1) {
+		for (let i: number=0; i<this.elemQueries.length; i+=1) {
 			let elems: NodeListOf<HTMLElement> = document.querySelectorAll(this.elemQueries[i]);
 			for (let j=0; j<elems.length; j+=1) {
 				elems[j].style.transition = this.transition;
@@ -33,25 +33,25 @@ class ColorCycles {
 
 
 // example implementation
-let exampleColors = new ColorCycles({
+let exampleColors: ColorCycles = new ColorCycles({
 	colors: ["pink", "violet", "orange", "paleturquoise", "lightgreen", "aqua", "plum", "peachpuff"],
 	pause: 500,
 	transition: '0.5s',
 	elemQueries: ["#example"],
 });
-let myCustomFont = new ColorCycles({
+let myCustomFont: ColorCycles = new ColorCycles({
 	colors: ["yellow", "pink", "teal", "springgreen", "aqua", "orange", "fuchsia", "lavender"],
 	pause: 1000,
 	transition: '0.5s',
 	elemQueries: ["#testText"],
 });
-let myCustomFont2 = new ColorCycles({
+let myCustomFont2: ColorCycles = new ColorCycles({
 	colors: ["rgb(200,150,60)", "rgb(190,50,230)", "rgb(230,210,240)", "rgb(20,230,240)", "rgb(250,200,200)", "rgb(190,190,70)"],
 	pause: 500,
 	transition: '0.3s',
 	elemQueries: ["#testText2", "#testText3"],
 });
-let myCustomFont3 = new ColorCycles({
+let myCustomFont3: ColorCycles = new ColorCycles({
 	colors: ["rgb(20,20,20)", "rgb(60,60,60)", "rgb(100,100,100)", "rgb(140,140,140)", "rgb(180,180,180)", "rgb(220,220,220)", "rgb(180,180,180)", "rgb(140,140,140)", "rgb(100,100,100)", "rgb(60,60,60)"],
 	pause: 300,
 	transition: '0.3s',
